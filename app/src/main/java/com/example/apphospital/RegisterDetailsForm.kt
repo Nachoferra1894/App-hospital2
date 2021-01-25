e341fcc5c5b82785f49384c8fba1ec2ce42f2fe0
agustinVon <agustin.von@ing.austral.edu.ar>
2021-01-24 23:13:25 -0300

    register slider form created

---
+++ app/src/main/java/com/example/apphospital/RegisterDetailsForm.kt
@@ -0,0 +1,38 @@
+package com.example.apphospital
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Html
+import android.widget.TextView
+import androidx.viewpager.widget.ViewPager
+import kotlinx.android.synthetic.main.activity_register_details_form.*
+
+class RegisterDetailsForm : AppCompatActivity() {
+
+    private lateinit var sliderAdapter:SliderAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_details_form)
+        sliderAdapter= SliderAdapter(this)
+
+        regform_slideview.adapter = sliderAdapter
+
+        addDots()
+    }
+
+    private fun addDots(){
+
+        val mDots = Array(3) { TextView(this) }
+
+        for (text in mDots){
+            text.text = Html.fromHtml("&#8226")
+            text.textSize = 35F
+            text.setTextColor(resources.getColor(R.color.teal_700))
+
+            regform_ll_mdot.addView(text)
+        }
+
+    }
+
+}
