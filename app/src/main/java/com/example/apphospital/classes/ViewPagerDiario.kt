41ab2d95e7bcccc1ef8e288757ba4c684e1d888f
Nacho Ferrari <ignacio.ferrari@ing.austral.edu.ar>
2021-01-28 00:45:09 -0300

    item1_registrodiario

---
+++ app/src/main/java/com/example/apphospital/classes/ViewPagerDiario.kt
@@ -0,0 +1,12 @@
+package com.example.apphospital.classes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.apphospital.R
+
+class ViewPagerDiario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_pager_diario)
+    }
+}
